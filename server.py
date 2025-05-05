761f9af0af111e09b9e74b2cff5719c3e68e1a1d
angedesu <104692309+angedesu@users.noreply.github.com>
2025-05-04 21:38:49 -0700

    connect to neon

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import sys
 import socket
+import psycopg2
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.bind(("0.0.0.0", 1234))
@@ -9,14 +10,27 @@ ip, port = socket.getsockname()
 print(f"Server created: {ip}:{port}")
 incomingSocket, incomingAddress = socket.accept()
 print("Got connection from", incomingAddress)
+connection = psycopg2.connect(
+    "postgresql://neondb_owner:npg_wbSvC7qAEGH3@ep-divine-meadow-a5w9f31z.us-east-2.aws.neon.tech/neondb?sslmode=require"
+)
+
+cursor = connection.cursor()
+
+
+
 while True:
     data = str(incomingSocket.recv(1024).decode())
     print(f"Received: {data}")
 
     if not data:
+        cursor.close()
+        connection.close()
         break
 
-    data = data.upper()
+    # data = data.upper()
+
+
+    # need functions for the queries 
     incomingSocket.send(bytearray(str(data), encoding="utf-8"))
     print("Sent", data)
 
