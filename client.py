468304bfdda5a44dc84cba3b2a9ee40492a2b4ae
huyvu203 <168234998+huyvu203@users.noreply.github.com>
2025-05-06 16:20:57 -0700

    fixed client server connection, working on neondb connection

--- client.py
+++ client.py
@@ -14,30 +14,20 @@ except:
 
 while True:
     # Get user query selection
-    try: 
-        user_query = int(input(("\nEnter 1, 2, or 3 to choose between these three query options:\n"
+    user_query = int(input(("\nEnter 1, 2, or 3 to choose between these three query options:\n"
                         "1. What is the average moisture inside my kitchen fridge in the past three hours?\n"
                         "2. What is the average water consumption per cycle in my smart dishwasher?\n"
-                        "3. Which device consumed more electricity among my three IoT devices (two refridgerators and a dishwasher)?")))
-    except:
-        print("Sorry, this query cannot be processed. Please try inputing 1, 2, or 3.")
-        continue
+                        "3. Which device consumed more electricity among my three IoT devices (two refridgerators and a dishwasher)?\n")))
+    
 
     # Process user query
-    if user_query == 1:
-        print("Query 1 selected")
-        # Do stuff here
-    elif user_query == 2:
-        print("Query 2 selected")
-    elif user_query == 3:
-        print("Query 3 selected")
-    else: 
+    if user_query not in [1, 2, 3]:
         print("Sorry, this query cannot be processed. Please try inputing 1, 2, or 3.")
-
-    # Send query selection to server
-    socket.send(bytearray(user_query, encoding='utf-8'))
-    response = socket.recv(1024).decode('utf-8')
-    print("Response:", response)
+    else:
+        # Send query selection to server
+        socket.send(bytearray(str(user_query), encoding='utf-8'))
+        response = socket.recv(1024).decode('utf-8')
+        print("Response:", response)
 
 socket.close()
 
--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import psycopg2
 import json
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-socket.bind(("0.0.0.0", 1234))
+socket.bind(("127.0.0.1", 1234))
 socket.listen(5)
 
 ip, port = socket.getsockname()
@@ -18,41 +18,11 @@ connection = psycopg2.connect(
 print("Connecting to database...")
 cursor = connection.cursor()
 
-
-
-while True:
-    data = int(incomingSocket.recv(1024).decode())
-    print(f"Received: {data}")
-
-    if not data:
-        cursor.close()
-        connection.close()
-        break
-
-    # data = data.upper()
-    if data == 1:
-        results = option_1()
-        
-    elif data == 2:
-        # method for querying for #2
-        results = 2
-
-    else:
-        #method for querying for #3
-        results = 3
-
-
-    # need functions for the queries 
-    incomingSocket.send(bytearray(str(results), encoding="utf-8"))
-    print("Sent", results)
-
-incomingSocket.close()
-
 def option_1():
     # connection to database
-    cursor.execute('SELECT payload FROM "IOTdata_virutal" WHERE payload->>\'board_name\' = \'fridge_board_1\'AND "time" >= NOW() - INTERVAL \'3 hours\'')
+    cursor.execute('SELECT payload FROM "IOTdata_virtual" WHERE payload->>\'board_name\' = \'fridge_board_1\'AND "time" >= NOW() - INTERVAL \'3 hours\'')
     
-    rows = cursor.fethall()
+    rows = cursor.fetchall()
     connection.close()
 
     numbers = []
@@ -71,3 +41,42 @@ def option_2():
 
 def option_3():
     return {"You have chosen option 3"}
+
+while True:
+    data = str(incomingSocket.recv(1024).decode())
+    print(f"Received: {data}")
+
+    '''if not data:
+        cursor.close()
+        connection.close()
+        break'''
+
+    
+    if data == "1":
+        print("Received option 1")
+        results = option_1()
+        
+        
+    elif data == "2":
+        print("Received option 2")
+        # method for querying for #2
+        results = 2
+
+    elif data == "3":
+        print("Received option 3")
+        #method for querying for #3
+        results = 3
+    
+    else:
+        print("Wrong query option")
+    
+
+
+
+    # need functions for the queries 
+    incomingSocket.send(bytearray(str(results), encoding="utf-8"))
+    print("Sent", results)
+
+incomingSocket.close()
+
+
