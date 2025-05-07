34568e4164e1ec33e15a661427b66d72e8b17897
huyvu203 <168234998+huyvu203@users.noreply.github.com>
2025-05-06 23:07:09 -0700

    fix user query input

--- client.py
+++ client.py
@@ -14,18 +14,18 @@ except:
 
 while True:
     # Get user query selection
-    user_query = int(input(("\nEnter 1, 2, or 3 to choose between these three query options:\n"
+    user_query = input(("\nEnter 1, 2, or 3 to choose between these three query options:\n"
                         "1. What is the average moisture inside my kitchen fridge in the past three hours?\n"
                         "2. What is the average water consumption per cycle in my smart dishwasher?\n"
-                        "3. Which device consumed more electricity among my three IoT devices (two refridgerators and a dishwasher)?\n")))
+                        "3. Which device consumed more electricity among my three IoT devices (two refridgerators and a dishwasher)?\n"))
     
 
     # Process user query
-    if user_query not in [1, 2, 3]:
+    if user_query not in ["1", "2", "3"]:
         print("Sorry, this query cannot be processed. Please try inputing 1, 2, or 3.")
     else:
         # Send query selection to server
-        socket.send(bytearray(str(user_query), encoding='utf-8'))
+        socket.send(bytearray(user_query, encoding='utf-8'))
         response = socket.recv(1024).decode('utf-8')
         print("Response:", response)
 
